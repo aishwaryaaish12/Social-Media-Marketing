651e61f6e756795c2d3eb3f3bf8421c42042bc4a
aishwaryaaish12 <aishwaryamanikandan035@gmail.com>
2024-03-01 18:27:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Container } from '@mui/material';
+import './App.css';
+import Login from './Components/Login/Login';
+import Navbar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <Container>
+      <Navbar />
+<Login/>
+</Container>
+  )
+}
+
+export default App;
